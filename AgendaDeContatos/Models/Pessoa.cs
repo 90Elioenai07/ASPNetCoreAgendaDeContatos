8656be3bcb5dfa6349d3f1f33cab67609924462b
Elioenai Oliveira Costa <elioenai.tecnologo@gmail.com>
2021-01-07 11:20:55 -0300

    Segundo commit

---
+++ AgendaDeContatos/Models/Pessoa.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AgendaDeContatos.Models
+{
+    public class Pessoa
+    {
+        public int PessoaId { get; set; }
+
+        [Required(ErrorMessage = "{0} é obrigatório")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "{0} é obrigatório")]
+        [EmailAddress(ErrorMessage = "Email Invalido")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "{0} é obrigatório")]
+        public string Telefone { get; set; }
+    }
+}
